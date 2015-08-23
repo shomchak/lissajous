85a64f9184ccbd6a4e8ad2ac71d771ca61684c68
Shomik Chakravarty <shomik.chakravarty@gmail.com>
2015-08-22 18:49:02 -0700

    Create AudioStream

--- lissajous/__init__.py
+++ lissajous/__init__.py
@@ -1,33 +1,49 @@
-import numpy
-import bisect
+from pyaudio import PyAudio
+import numpy as np
+from bisect import bisect_left
 
 
 class Analyzer(object):
 
-    def __init__(self, sample_rate, dtype=numpy.int16):
+    def __init__(self, sample_rate, dtype=np.int16):
         assert sample_rate % 2 == 0, 'sample_rate must be even'
         self.sample_rate = sample_rate
         self.sample_freq = 1.0 / sample_rate
         self.dtype = dtype
-        self.low_bounds = (0, 50)
-        self.mid_bounds = (50, 3000)
+        self.low_bounds = (0, 60)
+        self.mid_bounds = (60, 3000)
         self.high_bounds = (3000, 10000)
         self._bytes = None
         self._fft = None
 
     def load(self, bytes):
-        self._signal = numpy.fromstring(bytes, self.dtype)
+        self._signal = np.fromstring(bytes, self.dtype)
         self._fft, self._freqs = self._calculate_fft(self._signal)
 
     def _calculate_fft(self, signal):
-        fft = numpy.abs(numpy.fft.rfft(signal))
+        fft = np.abs(np.fft.rfft(signal))
         n = len(signal)
-        freqs = numpy.fft.fftfreq(n, self.sample_freq)[:n/2]
+        freqs = np.fft.fftfreq(n, self.sample_freq)[:n/2]
         return fft, freqs
 
     @property
     def power(self):
-        return numpy.log(numpy.sum(self._fft)/len(self._fft)/1000)
+        return self._power(self._fft)
+
+    @property
+    def low_power(self):
+        return self._power(self.lows[0])
+
+    @property
+    def mid_power(self):
+        return self._power(self.mids[0])
+
+    @property
+    def high_power(self):
+        return self._power(self.highs[0])
+
+    def _power(self, freqs):
+        return np.log(np.sum(freqs)/len(freqs)/1000)
 
     @property
     def freqs(self):
@@ -50,20 +66,62 @@ class Analyzer(object):
         return self._filter_fft(self.high_bounds)
 
     def _filter_fft(self, bounds):
-        low = bisect.bisect_left(self.freqs, bounds[0])
-        high = bisect.bisect_left(self.freqs, bounds[1])
+        low = bisect_left(self.freqs, bounds[0])
+        high = bisect_left(self.freqs, bounds[1])
         return self.fft[low:high], self.freqs[low:high]
 
     @property
     def low_peak(self):
         lows, low_freqs = self.lows
-        return low_freqs[numpy.argmax(lows)]
+        return low_freqs[np.argmax(lows)]
 
     def mid_peaks(self, n=1):
         mids, mid_freqs = self.mids
-        return mid_freqs[numpy.argpartition(-mids, n)[:n]]
+        return mid_freqs[np.argpartition(-mids, n)[:n]]
 
     @property
     def high_peak(self):
         highs, high_freqs = self.highs
-        return high_freqs[numpy.argmax(highs)]
+        return high_freqs[np.argmax(highs)]
+
+
+class AudioStream(object):
+
+    def __init__(self, sample_rate=44100, channels=1, width=2, chunk=1024,
+                 input_device_index=None):
+        self.sample_rate = sample_rate
+        self.channels = channels
+        self.width = width
+        self.chunk = chunk
+        self.input_device_index = input_device_index
+
+    def __enter__(self):
+        self._pa = PyAudio()
+        if self.input_device_index is None:
+            self.input_device_index = \
+                self._pa.get_default_input_device_info()['index']
+        self._stream = self._pa.open(
+            format=self._pa.get_format_from_width(self.width),
+            channels=self.channels,
+            rate=self.sample_rate,
+            input=True,
+            frames_per_buffer=self.chunk,
+            input_device_index=self.input_device_index)
+        self._stream.start_stream()
+        return self
+
+    def stream(self):
+        try:
+            while True:
+                bytes = self._stream.read(self.chunk)
+                self.handle(bytes)
+        except (KeyboardInterrupt, SystemExit):
+            pass
+
+    def __exit__(self, type, value, traceback):
+        self._stream.stop_stream()
+        self._stream.close()
+        self._pa.terminate()
+
+    def handle(self, bytes):
+        pass
